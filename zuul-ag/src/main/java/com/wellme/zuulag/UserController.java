9c9d9813b531ff8924c63b2c365208312b7fda3e
Punit Mishra <punit2512@gmail.com>
2018-08-12 11:52:35 +0530

    fixed compilation issue.

--- zuul-ag/src/main/java/com/wellme/zuulag/UserController.java
+++ zuul-ag/src/main/java/com/wellme/zuulag/UserController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PatientController {
+public class UserController {
 	
-
 	@RequestMapping("/add")
 	public void addPatient(){
 //		patientRepo.save(patient);
